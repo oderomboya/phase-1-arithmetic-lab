b30ff133df9e33c1d66b47d537ef75860aa56bfa
oderomboya <davismboya3@gmail.com>
2023-03-20 21:41:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-
+function sayHi(){
+    console.log(Davies)
+}
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+
+Math.floor(Math.random() * 10) + 1;
+
+
+  const num3 =14
+  const num4 = 5
+  const mod = num3%num4
+  console.log(mod);
+
+
+  
+  const  numbers = [1, 2, 3, 4, 5, 20]
+    const max = Math.max(...numbers)
+ 
+
+
+  console.log((max))
